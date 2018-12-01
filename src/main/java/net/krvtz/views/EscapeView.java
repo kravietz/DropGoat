176f0c6eacf524ce5444392ccef9e94f83a05283
Pawel Krawczyk <pawel.krawczyk@hush.com>
2018-12-01 23:48:35 +0000

    comment

--- src/main/java/net/krvtz/views/EscapeView.java
+++ src/main/java/net/krvtz/views/EscapeView.java
@@ -3,6 +3,7 @@ package net.krvtz.views;
 import io.dropwizard.views.View;
 
 public class EscapeView extends View {
+    // simplest possible usage of Freemarker template in Dropwizard view
     private final String content;
 
     public EscapeView(String content) {
