01836d5d050160798122e7289adc440cc9ca6a3e
Pawel Krawczyk <pawel.krawczyk@hush.com>
2018-12-01 23:49:53 +0000

    add XSS link

--- src/main/java/net/krvtz/resources/TagEncoderResource.java
+++ src/main/java/net/krvtz/resources/TagEncoderResource.java
@@ -10,15 +10,16 @@ import javax.ws.rs.core.Response;
 @Produces({MediaType.TEXT_HTML})
 @Consumes({MediaType.MULTIPART_FORM_DATA})
 public class TagEncoderResource {
-    static final String MODULE = "TagEncoderResource";
+    private static final String MODULE = "TagEncoderResource";
 
     @GET
     public Response handle(@QueryParam("input") String input) {
         String response = "<html><head><title>" + MODULE + "</title></head>" +
                 "<body><h1>" + MODULE + "</h1><div>" +
-                Encode.forHtml(input) +
+                "input=" + Encode.forHtml(input) +
                 "</div>" +
-                "<form><input name=input></form>" +
+                "<div><form><input name=input></form></div>" +
+                "<div><a href=\"?input=<script>alert(1)</script>\">xss</a></div>" +
                 "</body></html>";
         // The X-Xss-Protection header is disabled to prevent the XSS vector being blocked by the
         // browser's built-in xSS auditor
