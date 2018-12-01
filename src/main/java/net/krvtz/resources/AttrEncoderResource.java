79de9cfabf869d484d478b70a88d715cb0ab110d
Pawel Krawczyk <pawel.krawczyk@hush.com>
2018-12-01 23:46:12 +0000

    add XSS link

--- src/main/java/net/krvtz/resources/AttrEncoderResource.java
+++ src/main/java/net/krvtz/resources/AttrEncoderResource.java
@@ -11,17 +11,16 @@ import javax.ws.rs.core.Response;
 @Consumes({MediaType.MULTIPART_FORM_DATA})
 public class AttrEncoderResource {
 
-    static final String MODULE = "AttrEncoderResource";
+    private static final String MODULE = "AttrEncoderResource";
 
     @GET
     public Response handle(@QueryParam("input") String input) {
-        // TODO: switch to https://github.com/spullara/mustache.java
 
         String response = "<html><head><title>" + MODULE + "</title></head>" +
-                "<body><h1>" + MODULE + "</h1><div class=\"" +
-                Encode.forHtmlAttribute(input) +
-                "\">div contents</div>" +
-                "<form><input name=input></form>" +
+                "<body><h1>" + MODULE + "</h1>" +
+                "<div class=\"" + Encode.forHtmlAttribute(input) + "\">This &lt;div&gt; is attacked</div>" +
+                "<div><form><input name=input></form></div>" +
+                "<div><a href=\"?input=%22><script>alert(1)</script><div>\">xss</a></div>" +
                 "</body></html>";
         // The X-Xss-Protection header is disabled to prevent the XSS vector being blocked by the
         // browser's built-in xSS auditor
