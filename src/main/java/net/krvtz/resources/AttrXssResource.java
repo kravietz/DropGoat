d3fd6a7fb033b1779edee9015eb4dd2099825edd
Pawel Krawczyk <pawel.krawczyk@hush.com>
2019-02-11 16:06:01 +0000

    Migrate JS and attribute to HtmlView

--- src/main/java/net/krvtz/resources/AttrXssResource.java
+++ src/main/java/net/krvtz/resources/AttrXssResource.java
@@ -1,26 +1,17 @@
 package net.krvtz.resources;
 
+import net.krvtz.views.HtmlView;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("/attr-xss")
 @Produces({MediaType.TEXT_HTML})
 @Consumes({MediaType.MULTIPART_FORM_DATA})
 public class AttrXssResource {
 
-    static final String MODULE = "AttrXssResource";
-
     @GET
-    public Response handle(@QueryParam("input") String input) {
-        // TODO: switch to https://github.com/spullara/mustache.java
-
-        String response = "<html><head><title>" + MODULE + "</title></head>" +
-                "<body><h1>" + MODULE + "</h1><div class=\"" + input + "\">div contents</div>" +
-                "<form><input name=input></form>" +
-                "</body></html>";
-        // The X-Xss-Protection header is disabled to prevent the XSS vector being blocked by the
-        // browser's built-in xSS auditor
-        return Response.ok(response).encoding("utf-8").header("X-Xss-Protection", "0").build();
+    public HtmlView handle(@QueryParam("input") String input) {
+        return new HtmlView("/AttrXss.ftl.html", input);
     }
 }
